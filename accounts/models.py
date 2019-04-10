fd3c39decc97b4f0cf850eb77d694f18d42774fc
Basil Jose <basiljose1@gmail.com>
2019-04-11 00:27:35 +0530

    First commit with basic project setup

---
+++ accounts/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+# https://github.com/egorsmkv/simple-django-login-and-register
+
+class ModelManager(models.Manager):
+    def get_queryset(self, fetch_all=False):
+        return super(ModelManager, self).get_queryset() if fetch_all \
+            else super(ModelManager, self).get_queryset().filter(deleted_at__isnull=True)
+
+
+class DateMixin(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = ModelManager()
+
+    class Meta:
+        abstract = True
+
+
+class UserManager(ModelManager, BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin, DateMixin):
+
+    username = models.CharField(_('Username'),max_length=150, unique=True)
+    email = models.EmailField(_('Email address'), unique=True)
+    name = models.CharField(_('Name'), max_length=30)
+    is_active = models.BooleanField(_('Active'), default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        ordering = ['-created_at']
+
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
